768e5d11dd22b09e5fb109a0266826706a7353e6
ldb477 <ldb477@gmail.com>
2022-11-07 22:04:16 -0500

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -1,37 +1,6 @@
 import pyaudio
 import wave
-import sys
 
-class PlayAudioFile:
-    CHUNK = 1024
-
-    def __init__(self, file):
-        """ Init audio stream """ 
-        self.wf = wave.open(file, 'rb')
-        self.p = pyaudio.PyAudio()
-        self.stream = self.p.open(
-            format = self.p.get_format_from_width(self.wf.getsampwidth()),
-            channels = self.wf.getnchannels(),
-            rate = self.wf.getframerate(),
-            output = True
-        )
-
-    def play(self):
-        """ Play entire file """
-        data = self.wf.readframes(self.CHUNK)
-        while data != '':
-            self.stream.write(data)
-            data = self.wf.readframes(self.CHUNK)
-
-    def close(self):
-        """ Graceful shutdown """ 
-        self.stream.close()
-        self.p.terminate()
-
-# Usage example for pyaudio
-#a = AudioFile("1.wav")
-#a.play()
-#a.close()
 
 class RecordAudioFile:
 
@@ -86,26 +55,3 @@ class RecordAudioFile:
         wf.writeframes(b''.join(self.frames))
         wf.close()
 
-# Usage example for pyaudio
-#a = RecordAudioFile("1.wav")
-#a.recordInit()
-#while(something):
-#   a.recordContinue()    
-#a.recordStop()
-
-# def list_default_audio_devices(self):
-
-#     recordingDevicesList = r.get_default_input_device_info()
-#     playbackDevicesList = p.get_default_output_device_info()
-
-#     print("")
-
-#     for devices in recordingDevicesList:
-#         print(devices + " " + str(recordingDevicesList[devices]))
-
-#     print("")
-
-#     for devices in playbackDevicesList:
-#         print(devices + " " + str(playbackDevicesList[devices]))
-
-#     print("")
